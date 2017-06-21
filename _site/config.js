33f09f50df5f1a6aa8f0c7636df0cefa0b09e89c
shenbao <shenbaoone@gmail.com>
2017-06-21 10:03:16 +0800

    update

--- _site/config.js
+++ _site/config.js
@@ -1,22 +1,19 @@
 var CONFIG = {
   // your website's title
   document_title: "idoc-template",
-
+  // your website's url
+  site_url: "http://shenbao.github.io/idoc-template",
   // index page
   index: "README.md",
-
   // sidebar file
   sidebar_file: "sidebar.md",
-
-  // where the docs are actually stored on github - so you can edit
+  // github pro name
   base_url: "http://github.com/xx/xx/edit/gh-pages",
-
-  site_url: "http://shenbao.github.io/idoc-template",
-
+  // github url 
   github_url: "http://github.com/ShenBao/idoc-template",
-
+  // disqus Setting
   disqus_username: "ShenBao",
-
+  // hypercomments Setting
   hypercomments_userid: 91931
 };
 
